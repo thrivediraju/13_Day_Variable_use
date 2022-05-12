3b27d4715f6888c5a3a2c8e035a9c08802e9e147
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-05-12 15:35:00 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,42 @@
+"""
+Name: HD
+Date: 6th May 2022
+Day: Friday
+Time: 3:30-4:30PM
+File Description: About Variables
+"""
+''' The following are the variables which are valid '''
+# a=1
+# A=2
+# _b=3
+# _1=4
+# c_a=5
+# v_3=6
+# _=7
+# x_y3=8
+# r6=9
+# t_4_d=10 
+# print(A)
+# print(_b)
+# print(_1) 
+# print(c_a)
+# print(v_3)
+# print(_)
+# print(x_y3)
+# print(r6)
+# print(t_4_d)
+# print(ABC) # NameError: name 'ABC' is not defined
+
+''' The following are the rules which are not valid '''
+"""
+.a=5
+@=7
+7_@4=6
+$q=8
+2_=9
+r t=10
+f 
+"""
+# print('hello'.isidentifier())
+# print('4ello'.isidentifier())
+# print('_4ello'.isidentifier())
